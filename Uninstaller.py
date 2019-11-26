8319d0fc18b75c0a0df6a447c60ca1e8e628b99f
danielhornga <58226427+danielhornga@users.noreply.github.com>
2019-11-26 16:35:23 +0000

    Update Uninstaller.py

--- Uninstaller.py
+++ Uninstaller.py
@@ -7,7 +7,7 @@ path = os.getcwd()
 cfilepath = path + "/Config"
 afilepath = path + "/Assets"
 
-os.remove("ALPHAv1.py")
+os.remove("game-version-1.py")
 shutil.rmtree(cfilepath)
 shutil.rmtree(afilepath)
 os.remove("Uninstaller.py")
