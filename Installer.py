3e9b43e77c84351ac86b842069da229cfa2ec3ea
danielhornga <58226427+danielhornga@users.noreply.github.com>
2019-11-28 11:03:34 +0000

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -1,5 +1,6 @@
 ###Game Installer###
 
+##Imports##
 import os
 import uuid
 import subprocess
@@ -7,26 +8,69 @@ import sys
 import tkinter
 import urllib.request
 
-##tkinter window initialisation
+def Installation():
 
-subprocess.call([sys.executable, "-m", "pip", "install", "arcade"])
+    ##File Path Setup##
+    path = os.getcwd()
+    cfilepath = path + "/Config"
+    afilepath = path + "/Assets"
+    gpath = path + "/game-version-1.py"
+    upath = path + "/Uninstaller.py"
+    os.mkdir(cfilepath)
+    os.mkdir(afilepath)
 
-path = os.getcwd()
-cfilepath = path + "/Config"
-afilepath = path + "/Assets"
-gpath = path + "/game-version-1.py"
-upath = path + "/Uninstaller.py"
-os.mkdir(cfilepath)
-os.mkdir(afilepath)
+    ##Settings File Creation##
+    sfname = os.path.join(cfilepath, "Settings.txt")
+    f = open(sfname, "w+")
+    f.write("Screen_Height:800\nScreen_Width:800\nFPS:244")
+    f.close
 
-sfname = os.path.join(cfilepath, "Settings.txt")
-f = open(sfname, "w+")
-f.write("Screen_Height:800\nScreen_Width:800\nFPS:244")
-f.close
+    ##Program web downloads##
+    url = "https://raw.githubusercontent.com/danielhornga/game/master/game-version-1.py"
+    urllib.request.urlretrieve(url, gpath)
+    url = "https://raw.githubusercontent.com/danielhornga/game/master/Uninstaller.py"
+    urllib.request.urlretrieve(url, upath)
 
-url = 'https://raw.githubusercontent.com/danielhornga/game/master/game-version-1.py'
-urllib.request.urlretrieve(url, gpath)
+    ##Kill Tkinter Loading Screen##
+    LoadScreen.destroy()
 
-url = 'https://raw.githubusercontent.com/danielhornga/game/master/Uninstaller.py'
-urllib.request.urlretrieve(url, upath)
+def InstallationScreen():
 
+    ##Pass Required Global Variables##
+    global WindowWidth
+    global WindowHeight
+    global x
+    global y
+    global LoadScreen
+
+    ##Kill Tkinter Initial Menu##
+    InstallWindow.destroy()
+
+    ##Create 'Loading' Screen##
+    LoadScreen = tkinter.Tk()
+    LoadScreen.title("")
+    LoadScreen.geometry('%dx%d+%d+%d' % (WindowWidth, WindowHeight, x, y))
+    LoadScreen.resizable(0, 0)
+    LoadScreen.lift()
+    LoadScreen.call("wm", "attributes", ".", "-topmost", "1")
+    Installation()
+    LoadScreen.mainloop()
+
+##tkinter window initialisation##
+InstallWindow = tkinter.Tk()
+WindowWidth = 750
+WindowHeight = 500
+ScreenWidth = InstallWindow.winfo_screenwidth()
+ScreenHeight = InstallWindow.winfo_screenheight()
+x = (ScreenWidth/2) - (WindowWidth/2)
+y = (ScreenHeight/2) - (WindowHeight/2)
+InstallWindow.title("Game Installation Menu")
+InstallWindow.geometry('%dx%d+%d+%d' % (WindowWidth, WindowHeight, x, y))
+InstallWindow.resizable(0, 0)
+InstallWindow.lift()
+InstallWindow.call("wm", "attributes", ".", "-topmost", "1")
+ContinueButton = tkinter.Button(InstallWindow, text="Continue", command=InstallationScreen, height=1, width=10)
+ContinueButton.place(relx=.9, rely=.9, anchor="c")
+ExitButton = tkinter.Button(InstallWindow, text="Exit", command=InstallWindow.destroy, height=1, width=10)
+ExitButton.place(relx=.1, rely=.9, anchor="c")
+InstallWindow.mainloop()
