68e5737629018587cf59e7932700ea34831da3b3
danielhornga <58226427+danielhornga@users.noreply.github.com>
2019-12-04 17:19:06 +0000

    Update game-version-1.py

--- game-version-1.py
+++ game-version-1.py
@@ -1,1 +1,28 @@
-print("Test")
+##Game Patch Number##
+
+patchNumber = float(1.00)
+
+##Immediately Required Imports##
+
+import urllib.request
+import os
+
+##Fetch File For Most Recent Patch##
+
+path = os.getcwd()
+vLiveFilePath = path + "/Config/Live-Version.txt"
+url = "https://raw.githubusercontent.com/danielhornga/game/config/Live-Version"
+urllib.request.urlretrieve(url, vLiveFilePath)
+
+##Set Local File Path##
+vLocalFilePath = path + "/Config/Local-Version.txt"
+
+##Compare Fetched File To Local File##
+
+f = open(vLiveFilePath, "r")
+LiveVersion = float(f.readline())
+f.close
+
+f = open(vLocalFilePath, "r")
+LiveVersion = float(f.readline())
+f.close
