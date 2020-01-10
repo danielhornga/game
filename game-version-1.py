00a6015f5c69181edf4a0aa37904d26f80780e69
danielhornga <58226427+danielhornga@users.noreply.github.com>
2020-01-10 10:38:58 +0000

    Update game-version-1.py

--- game-version-1.py
+++ game-version-1.py
@@ -1,27 +1,70 @@
 ##Game Patch Number##
 
-patchNumber = float(1.00)
+patchNumber = float(1.01)
 
-##Immediately Required Imports##
+##Check if file "UpdateIncomplete.txt" is present##
+try:
+    f = open("UpdateIncomplete.txt", "r")
+except FileNotFoundError:
+    ##Immediately Required Imports##
 
-import urllib.request
-import os
+    import urllib.request
+    import os
+    import shutil
 
-##Fetch File For Most Recent Patch##
+    ##Fetch File For Most Recent Patch##
 
-path = os.getcwd()
-vLiveFilePath = path + "/Config/Live-Version.txt"
-url = "https://raw.githubusercontent.com/danielhornga/game/master/Live-Version"
-urllib.request.urlretrieve(url, vLiveFilePath)
+    path = os.getcwd()
+    vLiveFilePath = path + "/Config/Live-Version.txt"
+    url = "https://raw.githubusercontent.com/danielhornga/game/master/Live-Version"
+    urllib.request.urlretrieve(url, vLiveFilePath)
 
-##Compare Fetched File To Local Patch Number##
+    ##Compare Fetched File To Local Patch Number##
 
-f = open(vLiveFilePath, "r")
-LiveVersion = float(f.readline())
-f.close
+    f = open(vLiveFilePath, "r")
+    LiveVersion = float(f.readline())
+    f.close()
+    os.remove(vLiveFilePath)
 
-if LiveVersion != patchNumber:
-  ##Code to update here
+    if LiveVersion != patchNumber:
+        ##File Path Re-Setup##
+        cfilepath = path + "/Config"
+        afilepath = path + "/Assets"
+        gpath = path + "/game-version-1.py"
+        gnpath = path + "/game-version-1-U.py"
+        upath = path + "/Uninstaller.py"
+        SecondPhaseFP = path + "/UpdateIncomplete.txt"
+        shutil.rmtree(cfilepath)
+        shutil.rmtree(afilepath)
+        os.remove("Uninstaller.py")
+        os.mkdir(cfilepath)
+        os.mkdir(afilepath)
+
+        ##Settings File Creation##
+        sfname = os.path.join(cfilepath, "Settings.txt")
+        f = open(sfname, "w+")
+        f.write("Screen_Height:800\nScreen_Width:800\nFPS:244")
+        f.close()
+
+        ##Program web downloads##
+        url = "https://raw.githubusercontent.com/danielhornga/game/master/game-version-1.py"
+        urllib.request.urlretrieve(url, gnpath)
+        url = "https://raw.githubusercontent.com/danielhornga/game/master/Uninstaller.py"
+        urllib.request.urlretrieve(url, upath)
+
+        ##Relaunch and second phase pass##
+        f = open(SecondPhaseFP, "w+")
+        f.close()
+        os.system("game-version-1-U.py 1")
+        os.remove("game-version-1.py")
 
 else:
-  ##Game Code here##
+    f.close()
+    path = os.getcwd()
+    gpath = path + "/game-version-1.py"
+    gnpath = path + "/game-version-1-U.py"
+    os.rename(gnpath, gpath)
+    os.remove("UpdateIncomplete.txt")
+
+
+print("WORKING")
